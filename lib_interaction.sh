82cec6cf4f7db627d942b7a20c6920dcb557a727
tcneko <tc.neko@outlook.com>
2019-08-24 15:51:36 +0800

    v20190824_2

--- lib_interaction.sh
+++ lib_interaction.sh
@@ -5,34 +5,43 @@
 # last test environment: Ubuntu 18.04
 # description:
 
-if [[ "$flag_lib_interaction" -ne 0 ]]; then
+# to use this library, please add the following line to the main script
+# source $(dirname ${BASH_SOURCE[0]})/lib_interaction.sh
+# export flag_lib_interaction=0
 
-  # export flag_lib_interaction=0 # <-- add this to father script
+if [[ "$flag_lib_interaction" -ne 0 ]]; then
 
   echo_info() {
     echo -e "\e[1;32m[Info]\e[0m $@"
   }
+  export -f echo_info
 
   echo_warning() {
     echo >&2 -e "\e[1;33m[Warning]\e[0m $@"
   }
+  export -f echo_warning
 
   echo_error() {
     echo >&2 -e "\e[1;31m[Error]\e[0m $@"
   }
+  export -f echo_error
 
   echo_exit() {
     echo_error "$@"
     exit 1
   }
+  export -f echo_exit
 
   request_input() {
     echo "$2"
     eval "read -ep '>>> ' $1"
   }
+  export -f request_input
 
   request_yn() {
     prompt_info=$(echo "$1 (y/n)")
     request_input "$prompt_info" cho_yn
   }
+  export -f request_yn
+
 fi
--- lib_test.sh
+++ lib_test.sh
@@ -5,79 +5,97 @@
 # last test environment: Ubuntu 18.04
 # description:
 
-if [[ "$flag_lib_test" -ne 0 ]]; then
+# to use this library, please add the following line to the main script
+# source $(dirname ${BASH_SOURCE[0]})/lib_interaction.sh
+# export flag_lib_interaction=0
+# source $(dirname ${BASH_SOURCE[0]})/lib_test.sh
+# export flag_lib_test=0
 
-  # export flag_lib_test=0 # <-- add this to father script
-  source $(dirname ${BASH_SOURCE[0]})/lib_interaction.sh
+if [[ "$flag_lib_test" -ne 0 ]]; then
 
   test_cmd_fail() {
     if [ $? -ne 0 ]; then
       echo_warning "$@"
     fi
   }
+  export -f test_cmd_fail
 
   test_cmd_fail_exit() {
     if [ $? -ne 0 ]; then
       echo_exit "$@"
     fi
   }
+  export -f test_cmd_fail_exit
 
   test_cmd_success() {
     if [ $? -eq 0 ]; then
       echo_warning "$@"
     fi
   }
+  export -f test_cmd_success
 
   test_cmd_success_exit() {
     if [ $? -eq 0 ]; then
       echo_exit "$@"
     fi
   }
+  export -f test_cmd_success_exit
 
   test_root_exit() {
     if [ $(id -u) -ne 0 ]; then
       echo_exit 'Please run as root'
     fi
   }
+  export -f test_root_exit
 
   test_network_exit() {
     ping -c1 -W1 "$1" &>/dev/null
     test_cmd_fail_exit 'Please check network'
   }
+  export -f test_network_exit
 
   test_dir_x() {
     test -d "$1" -a -x "$1"
   }
+  export -f test_dir_x
 
   test_dir_x_exit() {
     test_dir_x "$1"
     test_cmd_fail_exit "$2"
   }
+  export -f test_dir_x_exit
+
   test_dir_w() {
     test -d "$1" -a -w "$1"
   }
+  export -f test_dir_w
 
   test_dir_w_exit() {
     test_dir_x "$1"
     test_cmd_fail_exit "$2"
   }
+  export -f test_dir_w_exit
 
   test_file_r() {
     test -f "$1" -a -r "$1"
   }
+  export -f test_file_r
 
   test_file_r_exit() {
     test_file_r "$1"
     test_cmd_fail_exit "$2"
   }
+  export -f test_file_r_exit
 
   test_file_w() {
     test -f "$1" -a -w "$1"
   }
+  export -f test_file_w
 
   test_file_w_exit() {
     test_file_w "$1"
     test_cmd_fail_exit "$2"
   }
+  export -f test_file_w_exit
 
 fi
