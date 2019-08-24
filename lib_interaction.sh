0d07c6f7933d3960f6d7ef1647e75ea0596645da
tcneko <tc.neko@outlook.com>
2019-08-24 15:25:09 +0800

    v20190824_1

--- lib_interaction.sh
+++ lib_interaction.sh
@@ -2,39 +2,37 @@
 
 # tcneko <tcneko@outlook.com>
 # create: 2018.08
-# last update: 2018.10
 # last test environment: Ubuntu 18.04
-# description: 
+# description:
 
-if [ -z "$include_lib_interaction" ]; then
+if [[ "$flag_lib_interaction" -ne 0 ]]; then
 
-    function echo_info() {
-        echo -e "\e[1;32m[Info]\e[0m $@"
-    }
+  # export flag_lib_interaction=0 # <-- add this to father script
 
-    function echo_warning() {
-        echo >&2 -e "\e[1;33m[Warning]\e[0m $@"
-    }
+  echo_info() {
+    echo -e "\e[1;32m[Info]\e[0m $@"
+  }
 
-    function echo_error() {
-        echo >&2 -e "\e[1;31m[Error]\e[0m $@"
-    }
+  echo_warning() {
+    echo >&2 -e "\e[1;33m[Warning]\e[0m $@"
+  }
 
-    function echo_exit() {
-        echo_error "$@"
-        exit 1
-    }
+  echo_error() {
+    echo >&2 -e "\e[1;31m[Error]\e[0m $@"
+  }
 
-    function request_input() {
-        echo "$2"
-        eval "read -ep '>>> ' $1"
-    }
+  echo_exit() {
+    echo_error "$@"
+    exit 1
+  }
 
-    function request_yn() {
-        prompt_info=$(echo "$1 (y/n)")
-        request_input "$prompt_info" cho_yn
-    }
+  request_input() {
+    echo "$2"
+    eval "read -ep '>>> ' $1"
+  }
 
-    include_lib_interaction='0'
-
+  request_yn() {
+    prompt_info=$(echo "$1 (y/n)")
+    request_input "$prompt_info" cho_yn
+  }
 fi
--- lib_test.sh
+++ lib_test.sh
@@ -2,84 +2,82 @@
 
 # tcneko <tcneko@outlook.com>
 # create: 2018.08
-# last update: 2018.10
 # last test environment: Ubuntu 18.04
 # description:
 
-if [ -z "$include_lib_test" ]; then
+if [[ "$flag_lib_test" -ne 0 ]]; then
 
-    source $(dirname ${BASH_SOURCE[0]})/lib_interaction.sh
+  # export flag_lib_test=0 # <-- add this to father script
+  source $(dirname ${BASH_SOURCE[0]})/lib_interaction.sh
 
-    function test_cmd_fail() {
-        if [ $? -ne 0 ]; then
-            echo_warning "$@"
-        fi
-    }
+  test_cmd_fail() {
+    if [ $? -ne 0 ]; then
+      echo_warning "$@"
+    fi
+  }
 
-    function test_cmd_fail_exit() {
-        if [ $? -ne 0 ]; then
-            echo_exit "$@"
-        fi
-    }
+  test_cmd_fail_exit() {
+    if [ $? -ne 0 ]; then
+      echo_exit "$@"
+    fi
+  }
 
-    function test_cmd_success() {
-        if [ $? -eq 0 ]; then
-            echo_warning "$@"
-        fi
-    }
+  test_cmd_success() {
+    if [ $? -eq 0 ]; then
+      echo_warning "$@"
+    fi
+  }
 
-    function test_cmd_success_exit() {
-        if [ $? -eq 0 ]; then
-            echo_exit "$@"
-        fi
-    }
+  test_cmd_success_exit() {
+    if [ $? -eq 0 ]; then
+      echo_exit "$@"
+    fi
+  }
 
-    function test_root_exit() {
-        if [ $(id -u) -ne 0 ]; then
-            echo_exit 'Please run as root'
-        fi
-    }
+  test_root_exit() {
+    if [ $(id -u) -ne 0 ]; then
+      echo_exit 'Please run as root'
+    fi
+  }
 
-    function test_network_exit() {
-        ping -c1 -W1 "$1" &>/dev/null
-        test_cmd_fail_exit 'Please check network'
-    }
+  test_network_exit() {
+    ping -c1 -W1 "$1" &>/dev/null
+    test_cmd_fail_exit 'Please check network'
+  }
 
-    function test_dir_x() {
-        test -d "$1" -a -x "$1"
-    }
+  test_dir_x() {
+    test -d "$1" -a -x "$1"
+  }
 
-    function test_dir_x_exit() {
-        test_dir_x "$1"
-        test_cmd_fail_exit "$2"
-    }
-    function test_dir_w() {
-        test -d "$1" -a -w "$1"
-    }
+  test_dir_x_exit() {
+    test_dir_x "$1"
+    test_cmd_fail_exit "$2"
+  }
+  test_dir_w() {
+    test -d "$1" -a -w "$1"
+  }
 
-    function test_dir_w_exit() {
-        test_dir_x "$1"
-        test_cmd_fail_exit "$2"
-    }
+  test_dir_w_exit() {
+    test_dir_x "$1"
+    test_cmd_fail_exit "$2"
+  }
 
-    function test_file_r() {
-        test -f "$1" -a -r "$1"
-    }
+  test_file_r() {
+    test -f "$1" -a -r "$1"
+  }
 
-    function test_file_r_exit() {
-        test_file_r "$1"
-        test_cmd_fail_exit "$2"
-    }
+  test_file_r_exit() {
+    test_file_r "$1"
+    test_cmd_fail_exit "$2"
+  }
 
-    function test_file_w() {
-        test -f "$1" -a -w "$1"
-    }
+  test_file_w() {
+    test -f "$1" -a -w "$1"
+  }
 
-    function test_file_w_exit() {
-        test_file_w "$1"
-        test_cmd_fail_exit "$2"
-    }
-
-    include_lib_test='0'
+  test_file_w_exit() {
+    test_file_w "$1"
+    test_cmd_fail_exit "$2"
+  }
 
 fi
